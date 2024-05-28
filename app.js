9f5edeb0eecd083121df4807ef0f1b50cae2ff95
Chelsea-san <chelseagabriellag@gmail.com>
2024-05-28 16:44:14 +0100

    Website update

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send(' this is CSS 314 cloud computing DEVOPS ; CI-CD PIPELINE. THIS PAGE INDICATES SUCCESS step1 !'));
+app.use(express.static("public"));
+
+//app.get('/', (req, res) => res.send(' THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WITH AWS THIS PAGE INDICATES SUCCESS'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
